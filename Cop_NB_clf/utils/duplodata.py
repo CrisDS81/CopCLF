65b71cbf7f7a1afcb1a96dac434c1c71488dab0f
CrisDS81 <46658540+CrisDS81@users.noreply.github.com>
2022-06-13 07:55:10 +0200

    Update duplodata.py

--- Cop_NB_clf/utils/duplodata.py
+++ Cop_NB_clf/utils/duplodata.py
@@ -10,13 +10,13 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def duplodata():
-    ts_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/train_0.npy')
-    ts_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/val_0.npy')
-    ts_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/test_0.npy')
+    ts_train = np.load('C:/reunion_learned_features/train_0.npy')
+    ts_val = np.load('C:/reunion_learned_features/val_0.npy')
+    ts_test = np.load('C:/reunion_learned_features/test_0.npy')
     
-    labels_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/train_y0_30.npy')
-    labels_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/validation_y0_20.npy')
-    labels_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/test_y0_30.npy')
+    labels_train = np.load('C:/ground_truth/train_y0_30.npy')
+    labels_val = np.load('C:/ground_truth/validation_y0_20.npy')
+    labels_test = np.load('C:/ground_truth/test_y0_30.npy')
     
     #merged_ts is dataset without labels
     
@@ -38,13 +38,13 @@ def duplodata():
 
 
 def duplodata_reduction():
-    ts_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/train_0.npy')
-    ts_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/val_0.npy')
-    ts_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/test_0.npy')
+    ts_train = np.load('C:/reunion_learned_features/train_0.npy')
+    ts_val = np.load('C:/reunion_learned_features/val_0.npy')
+    ts_test = np.load('C:/reunion_learned_features/test_0.npy')
     
-    labels_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/train_y0_30.npy')
-    labels_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/validation_y0_20.npy')
-    labels_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/test_y0_30.npy')
+    labels_train = np.load('C:/ground_truth/train_y0_30.npy')
+    labels_val = np.load('C:/ground_truth/validation_y0_20.npy')
+    labels_test = np.load('C:/ground_truth/test_y0_30.npy')
     
     #merged_ts is dataset without labels
     
@@ -69,13 +69,13 @@ def duplodata_reduction():
     return X_train, X_test, y_train, y_test
 
 def duplodata_reduction_1():#with stratified shuffle split (to complete!!)
-    ts_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/train_0.npy')
-    ts_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/val_0.npy')
-    ts_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/reunion_learned_features/test_0.npy')
+    ts_train = np.load('C:/reunion_learned_features/train_0.npy')
+    ts_val = np.load('C:/reunion_learned_features/val_0.npy')
+    ts_test = np.load('C:/reunion_learned_features/test_0.npy')
     
-    labels_train = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/train_y0_30.npy')
-    labels_val = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/validation_y0_20.npy')
-    labels_test = np.load('C:/Users/Cristiano/Desktop/Python_Code/CoDuPlo/Dataset/Duplo_Data/ground_truth/test_y0_30.npy')
+    labels_train = np.load('C:/ground_truth/train_y0_30.npy')
+    labels_val = np.load('C:/ground_truth/validation_y0_20.npy')
+    labels_test = np.load('C:/ground_truth/test_y0_30.npy')
     
     #merged_ts is dataset without labels
     
